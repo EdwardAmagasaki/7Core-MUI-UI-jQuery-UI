4da9df4750acf93e23e18f386ba81d278eef61ac
Edward Amagasaki <amagasaki.edward@gmail.com>
2023-04-09 20:23:09 -0300

    ..

---
+++ Controllers/DialogController.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using _7Core_MUI_UI_jQuery_UI.Models;
+
+namespace _7Core_MUI_UI_jQuery_UI.Controllers;
+
+public class DialogController : Controller
+{
+    private readonly ILogger<DialogController> _logger;
+
+    public DialogController(ILogger<DialogController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
